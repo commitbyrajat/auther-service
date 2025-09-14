27d5ec9b43bfc31fb941b2680828c141c5558f87
rajatnigam <rajatnigam89@gmail.com>
2025-09-14 21:09:58 +0530

    add more repository layer tests

--- src/test/java/com/hdfcbank/app/AuthorRepositoryTest.java
+++ src/test/java/com/hdfcbank/app/AuthorRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.hdfcbank.app;
 
-
+import com.hdfcbank.app.entities.Author;
+import com.hdfcbank.app.entities.Book;
 import com.hdfcbank.app.repositories.AuthorRepository;
 import com.hdfcbank.app.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
@@ -30,5 +31,64 @@ class AuthorRepositoryTest {
                 .verifyComplete();
     }
 
+    @Test
+    void testSaveAndFindAuthor() {
+        Author author = new Author();
+        author.setName("New Author");
 
+        StepVerifier.create(authorRepository.save(author)
+                        .flatMap(saved -> authorRepository.findById(saved.getId())))
+                .assertNext(found -> {
+                    assert found.getId() != null;
+                    assert found.getName().equals("New Author");
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void testSaveAndFindBook() {
+        Author author = new Author();
+        author.setName("Book Author");
+
+        StepVerifier.create(authorRepository.save(author)
+                        .flatMap(savedAuthor -> {
+                            Book book = new Book();
+                            book.setTitle("Reactive Spring");
+                            book.setAuthorId(savedAuthor.getId());
+                            return bookRepository.save(book)
+                                    .flatMap(savedBook -> bookRepository.findById(savedBook.getId()));
+                        }))
+                .assertNext(foundBook -> {
+                    assert foundBook.getId() != null;
+                    assert foundBook.getTitle().equals("Reactive Spring");
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void testFindAuthorById() {
+        StepVerifier.create(authorRepository.findById(1L))
+                .assertNext(author -> {
+                    assert author.getId() == 1L;
+                    assert author.getName() != null;
+                })
+                .verifyComplete();
+    }
+
+    @Test
+    void testDeleteAuthor() {
+        Author author = new Author();
+        author.setName("To Be Deleted");
+
+        StepVerifier.create(authorRepository.save(author)
+                        .flatMap(saved -> authorRepository.deleteById(saved.getId())
+                                .then(authorRepository.findById(saved.getId()))))
+                .verifyComplete(); // should emit no values
+    }
+
+    @Test
+    void testFindBooksByInvalidAuthor() {
+        StepVerifier.create(bookRepository.findByAuthorId(9999L))
+                .verifyComplete(); // no books should be returned
+    }
 }
